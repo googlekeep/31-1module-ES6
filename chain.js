1d3935e32417f0637cfd74b8d3c728ebe709fcdf
googlekeep <info.workingforyou@gmail.com>
2022-10-11 00:43:30 +0530

    data chain array to normal

---
+++ chain.js
@@ -0,0 +1,15 @@
+const users = [{
+    id: 1, name: 'abdul', job: 'doctor'
+}]
+console.log(users[0].name);
+
+
+const data = {
+    count: 5000,
+    data:[
+        {id:1, name:'abdul', job: 'leader'},
+        {id:3, name:'abhdgfegf', job: 'artader'}
+    ]
+}
+
+console.log(data.data[0].job);
